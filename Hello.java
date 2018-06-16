c3f1e4f148ceba4c1585acdfc2ec79d9f25501e7
Anand R <anandr72@gmail.com>
2018-06-16 18:09:11 +0530

    New Job

--- Hello.java
+++ Hello.java
@@ -2,7 +2,6 @@ public class Hello {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
       System.out.println("Hello, World");
-  	System.out.println("Automatic Build Triggered2");
-	System.out.println("Automatic Build Triggered from SpringPeople Lab");
+  	
    }
 }
