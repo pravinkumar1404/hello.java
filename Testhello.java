4f8623cf18d16d201ae3864005d2a07fc2cb3228
pravinkumar1404 <36580038+pravinkumar1404@users.noreply.github.com>
2018-08-30 17:24:25 +0530

    Create Testhello.java

---
+++ Testhello.java
@@ -0,0 +1,6 @@
+echo "# hello.java" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.dxc.com/pbangera2/hello.java.git
+git push -u origin master
